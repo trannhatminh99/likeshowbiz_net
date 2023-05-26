bc7c41a1a429cc79d35577dcd7d184921cad84bc
trannhatminh99 <50400029+trannhatminh99@users.noreply.github.com>
2023-05-26 18:03:42 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ function Home() {
     // 👇️ get ID from url
     const params = useParams();
     const id = params.id.split('-').slice(-1).pop();
-    window.location.href = "http://likeshowbiz.com/?p=" + id;
+    window.location.href = "http://likeshowbiz.net/?p=" + id;
 
 
     return <div/>;
