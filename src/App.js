bbd38982b4e77e21411dce209cafc3db2f68bb12
DESKTOP-B1QD8OP\hoang <hoang.nguyen@molinetwork.com>
2023-05-26 14:37:01 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+import React from "react";
 
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+          {this.props.children}
+        </div>
+    )
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,68 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
-import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+import { Router, Route, IndexRoute } from 'react-router'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-                {this.props.children}
-            </div>
-        )
-    }
-}
-export default App;
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+    },
+]);
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Home...</h1>
-            </div>
-        )
-    }
-}
-export default Home;
 
-class About extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>About...</h1>
-            </div>
-        )
-    }
-}
-export default About;
-
-class Contact extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Contact...</h1>
-            </div>
-        )
-    }
-}
-export default Contact;
 
 ReactDOM.render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     {/*<App />*/}
-      <Router history = {browserHistory}>
-          <Route path = "/" component = {App}>
-              <IndexRoute component = {Home} />
-              <Route path = "home" component = {Home} />
-              <Route path = "about" component = {About} />
-              <Route path = "contact" component = {Contact} />
-          </Route>
-      </Router>
+      {/*<Router>*/}
+      {/*    <Route path = "/" component = {App}>*/}
+      {/*        /!*<IndexRoute component = {Home} />*!/*/}
+      {/*        <Route path = "home" component = {Home} />*/}
+      {/*        <Route path = "about" component = {About} />*/}
+      {/*    </Route>*/}
+      {/*</Router>*/}
   </React.StrictMode>,
   document.getElementById('root')
 );
